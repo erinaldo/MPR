ecf9a0618034ebcc1eaffcf7d16e03a86ab29c5a
kamal.sharma721 <kamal.sharma721@gmail.com>
2018-01-01 12:58:36 +0530

    changes in script

--- Forms/frm_Item_Master.vb
+++ Forms/frm_Item_Master.vb
@@ -295,6 +295,7 @@ Public Class frm_Item_Master
                 Flag = "save"
             End If
             ds.Dispose()
+            txtSearch.Text = ""
         Catch ex As Exception
             MsgBox(ex.Message, MsgBoxStyle.Critical, "Error --> ")
         End Try
