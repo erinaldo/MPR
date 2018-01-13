2892ce7931eb86eb82c38a1c1846669c5f6ad8a3
DESKTOP-VCDO427\VT <thakurvishal.05@gmail.com>
2018-01-13 14:59:07 +0530

    hsn sheet added in gstr1 and date format...

--- ReportsForms/frm_GSTR_1.vb
+++ ReportsForms/frm_GSTR_1.vb
@@ -27,6 +27,7 @@ Public Class frm_GSTR_1
     Dim b2bTable As DataTable
     Dim b2clTable As DataTable
     Dim b2csTable As DataTable
+    Dim hsnTable As DataTable 
 
     Private Sub ImportData()
     	Dim path As String = Application.StartupPath + "\ExcelTemplate\GSTR1_Template.xlsx"
@@ -45,7 +46,9 @@ Public Class frm_GSTR_1
         Try        	
         WriteB2BData(xlWorkBook)
         WriteB2CLData(xlWorkBook)
-        WriteB2CSData(xlWorkBook)        
+        WriteB2CSData(xlWorkBook)      
+        WriteHsnData(xlWorkBook)
+        
         xlWorkBook.SaveAs(sfd.FileName)
         Finally        
         xlWorkBook.Close(SaveChanges:=False)
@@ -64,7 +67,7 @@ Public Class frm_GSTR_1
     		Dim invoiceNo As String = row("SI_CODE") + row("SI_NO").ToString()
     		xlWorkSheet.Cells(rowIndex , 1) = row("VAT_NO")
     		xlWorkSheet.Cells(rowIndex , 2) = invoiceNo
-    		xlWorkSheet.Cells(rowIndex , 3) = row("SI_DATE")
+    		xlWorkSheet.Cells(rowIndex , 3) = Convert.ToDateTime(row("SI_DATE")).ToString("dd-MMM-yy")
     		xlWorkSheet.Cells(rowIndex , 4) = invoiceAmount
     		xlWorkSheet.Cells(rowIndex , 5) = state
     		xlWorkSheet.Cells(rowIndex , 6) = "N"
@@ -110,6 +113,24 @@ Public Class frm_GSTR_1
     		rowIndex+=1
     	Next
     End Sub
+    
+    Private Sub WriteHsnData(xlWorkBook As Excel.Workbook)
+    	Dim rowIndex As Int32 = 5
+    	Dim xlWorkSheet As Excel.Worksheet = xlWorkBook.Worksheets("hsn")    	
+    	For Each row As DataRow In hsnTable.Rows 
+    		xlWorkSheet.Cells(rowIndex , 1) = row("HsnCode_vch")
+    		xlWorkSheet.Cells(rowIndex , 2) = row("ITEM_NAME")
+    		xlWorkSheet.Cells(rowIndex , 3) = row("UM_Name") + "-" + row("UM_DESC")
+    		xlWorkSheet.Cells(rowIndex , 4) = row("Qty")
+    		xlWorkSheet.Cells(rowIndex , 5) = row("Taxable_Value") + row("Cess_Amount") + row("non_integrated_tax") + row("integrated_tax")
+    		xlWorkSheet.Cells(rowIndex , 6) = row("Taxable_Value")
+    		xlWorkSheet.Cells(rowIndex , 7) = row("integrated_tax")
+    		xlWorkSheet.Cells(rowIndex , 8) = row("non_integrated_tax")/2
+    		xlWorkSheet.Cells(rowIndex , 9) = row("non_integrated_tax")/2
+    		xlWorkSheet.Cells(rowIndex , 10) = row("Cess_Amount")
+    		rowIndex+=1
+    	Next
+    End Sub
 
     Private Sub releaseObject(ByVal obj As Object)
         Try
@@ -121,8 +142,6 @@ Public Class frm_GSTR_1
             GC.Collect()
         End Try
     End Sub
-    
-
     
     Private Sub BindData()
 	    Dim QryTemplate As String = "SELECT VAT_NO,SI_CODE, SI_NO, SI_DATE, STATE_CODE,STATE_NAME,  " &
@@ -156,6 +175,22 @@ Public Class frm_GSTR_1
             " AND LEN(VAT_NO)=0 and inv.NET_AMOUNT<250000 " &
             " GROUP BY STATE_CODE,STATE_NAME,VAT_PER"                
         b2csTable = objCommFunction.Fill_DataSet(Qry).Tables(0)
+        
+        Qry = " SELECT HsnCode_vch, ITEM_NAME, UM_Name, UM_DESC, SUM(invd.BAL_ITEM_QTY) AS Qty, " &
+			" SUM(((BAL_ITEM_QTY * BAL_ITEM_RATE) - ISNULL(ITEM_DISCOUNT,0))) AS Taxable_Value, SUM(0) Cess_Amount," &
+			" SUM(CASE WHEN sm.STATE_ID =4 THEN invd.VAT_AMOUNT ELSE 0 END) AS non_integrated_tax," &
+			" SUM(CASE WHEN sm.STATE_ID !=4 THEN invd.VAT_AMOUNT ELSE 0 END) AS integrated_tax" &
+			" FROM    dbo.SALE_INVOICE_MASTER inv" &
+			" INNER JOIN dbo.SALE_INVOICE_DETAIL invd ON invd.SI_ID = inv.SI_ID" &
+			" INNER JOIN dbo.ITEM_MASTER im ON invd.ITEM_ID = im.ITEM_ID" &
+			" INNER JOIN dbo.UNIT_MASTER um ON um.UM_ID = im.UM_ID" &
+			" INNER JOIN dbo.ACCOUNT_MASTER am ON am.ACC_ID = inv.CUST_ID" &
+			" INNER JOIN dbo.CITY_MASTER cm ON cm.CITY_ID = am.CITY_ID" &
+			" INNER JOIN dbo.STATE_MASTER sm ON sm.STATE_ID = cm.STATE_ID" &
+			" INNER JOIN dbo.HsnCode_Master hsn ON hsn.Pk_HsnId_num = im.fk_HsnId_num" &
+			" WHERE MONTH(SI_DATE) =9 AND YEAR(SI_DATE)=2017 " &
+			" GROUP BY HsnCode_vch,ITEM_NAME, UM_Name,UM_DESC"
+        hsnTable = objCommFunction.Fill_DataSet(Qry).Tables(0)
     End Sub
 
     Public Sub NewClick(sender As Object, e As EventArgs) Implements IForm.NewClick
