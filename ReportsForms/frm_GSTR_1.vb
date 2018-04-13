091fa3eccbff458075bc54e6e461aee05b43d297
unknown <Administrator@system3.syncsolzlocal.com>
2018-04-13 12:05:20 +0530

    Null check in docsreport in GSTR1

--- ReportsForms/frm_GSTR_1.vb
+++ ReportsForms/frm_GSTR_1.vb
@@ -485,13 +485,13 @@ Public Class frm_GSTR_1
 
         exempTable = objCommFunction.Fill_DataSet(Qry).Tables(0)
 
-        Qry = "SELECT 'Invoice for outward supply' AS Description,  MIN(SI_NO) AS 'Sr. No. From', MAX(SI_NO) AS 'Sr. No. To', COUNT(SI_No) AS 'Total Number', " &
+        Qry = "SELECT 'Invoice for outward supply' AS Description,  Isnull(MIN(SI_NO),0) AS 'Sr. No. From', Isnull(MAX(SI_NO),0) AS 'Sr. No. To', COUNT(SI_No) AS 'Total Number', " &
         "  ( SELECT  COUNT(SI_NO) FROM  dbo.SALE_INVOICE_MASTER WHERE MONTH(SI_DATE) =" + txtFromDate.Value.Month.ToString() &
         "   AND YEAR(SI_DATE) = " + txtFromDate.Value.Year.ToString() &
         "  AND INVOICE_STATUS = 4) AS Cancelled FROM  dbo.SALE_INVOICE_MASTER WHERE MONTH(SI_DATE) = " + txtFromDate.Value.Month.ToString() &
         "  AND YEAR(SI_DATE) = " + txtFromDate.Value.Year.ToString() &
         "  UNION ALL " &
-        "  SELECT 'Credit Note' AS Description, MIN(CreditNote_No) AS 'Sr. No. From', MAX(CreditNote_No) AS 'Sr. No. To', COUNT(CreditNote_No) AS 'Total Number', " &
+        "  SELECT 'Credit Note' AS Description, Isnull(MIN(CreditNote_No),0) AS 'Sr. No. From', Isnull(MAX(CreditNote_No),0) AS 'Sr. No. To', COUNT(CreditNote_No) AS 'Total Number', " &
         "  0 AS Cancelled FROM  dbo.CreditNote_Master WHERE MONTH(CreditNote_Date) = " + txtFromDate.Value.Month.ToString() &
         "  AND YEAR(CreditNote_Date) = " + txtFromDate.Value.Year.ToString()
 
