5c23c56dd98a753899fdfd1b710cad5fdd60dcf2
kamals2 <kamal.sharma721@gmail.com>
2018-04-13 13:23:43 +0530

    bug fixing in GSTR 3

--- ReportsForms/frm_GSTR_3.vb
+++ ReportsForms/frm_GSTR_3.vb
@@ -17,7 +17,7 @@ Public Class frm_GSTR_3
     End Sub
 
     Private Sub frm_GSTR_3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        stateId = objCommFunction.ExecuteScalar("SELECT STATE_ID FROM dbo.OutletMaster om JOIN dbo.CITY_MASTER cm ON om.fk_CityId_num = cm.CITY_ID")
+        stateId = objCommFunction.ExecuteScalar("SELECT STATE_ID FROM dbo.CITY_MASTER WHERE CITY_ID IN(SELECT TOP 1 CITY_ID FROM dbo.DIVISION_SETTINGS)")
     End Sub
 
     Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
@@ -64,30 +64,30 @@ Public Class frm_GSTR_3
         xlWorkSheet.Cells(4, 15) = txtFromDate.Value.ToString("yyyy")
         xlWorkSheet.Cells(5, 15) = txtFromDate.Value.ToString("MMMM")
         Dim colIndex As Int16 = 2
-        For Each ch As Char In row("TinNo_vch").ToString
+        For Each ch As Char In row("TIN_NO").ToString
             xlWorkSheet.Cells(6, colIndex) = ch.ToString
             colIndex += 1
         Next
-        xlWorkSheet.Cells(7, 2) = row("OutletName_vch")
+        xlWorkSheet.Cells(7, 2) = row("DIVISION_NAME")
         Return xlWorkSheet
     End Function
 
     Private Function Add3_1SectionData(xlWorkSheet As Excel.Worksheet) As Excel.Worksheet
-        row = Get3_1_SectionData(" And LEN(am.VAT_NO) > 0 AND (invd.VAT_PER)>0")
+        row = Get3_1_SectionData(" And LEN(ISNUll(am.VAT_NO,'')) > 0 AND (invd.VAT_PER)>0")
         xlWorkSheet.Cells(11, 2) = row("Taxable_Value")
         xlWorkSheet.Cells(11, 5) = row("integrated_tax")
         xlWorkSheet.Cells(11, 8) = row("non_integrated_tax") / 2
         xlWorkSheet.Cells(11, 11) = row("non_integrated_tax") / 2
         xlWorkSheet.Cells(11, 14) = row("Cess_Amount")
 
-        row = Get3_1_SectionData(" And LEN(am.VAT_NO) > 0 AND (invd.VAT_PER)=0")
+        row = Get3_1_SectionData(" And LEN(ISNUll(am.VAT_NO,'')) > 0 AND (invd.VAT_PER)=0")
         xlWorkSheet.Cells(13, 2) = row("Taxable_Value")
         xlWorkSheet.Cells(13, 5) = row("integrated_tax")
         xlWorkSheet.Cells(13, 8) = row("non_integrated_tax") / 2
         xlWorkSheet.Cells(13, 11) = row("non_integrated_tax") / 2
         xlWorkSheet.Cells(13, 14) = row("Cess_Amount")
 
-        row = Get3_1_SectionData(" And LEN(am.VAT_NO) = 0 ")
+        row = Get3_1_SectionData(" And LEN(ISNUll(am.VAT_NO,'')) = 0 ")
         xlWorkSheet.Cells(15, 2) = row("Taxable_Value")
         xlWorkSheet.Cells(15, 5) = row("integrated_tax")
         xlWorkSheet.Cells(15, 8) = row("non_integrated_tax") / 2
@@ -155,8 +155,8 @@ Public Class frm_GSTR_3
 
     Private Function Get5SectionData() As DataRow
         Qry = " SELECT " &
-             " SUM(CASE WHEN sm.STATE_ID = " & stateId & " THEN ((BAL_ITEM_QTY * BAL_ITEM_RATE) - ISNULL(ITEM_DISCOUNT,0)) ELSE 0 END) AS IntraState_TaxableValue, " &
-             " SUM(CASE WHEN sm.STATE_ID != " & stateId & " THEN ((BAL_ITEM_QTY * BAL_ITEM_RATE) - ISNULL(ITEM_DISCOUNT,0)) ELSE 0 END) AS InterState_TaxableValue " &
+             " SUM(CASE WHEN inv.INV_TYPE <>'I' THEN ((BAL_ITEM_QTY * BAL_ITEM_RATE) - ISNULL(ITEM_DISCOUNT,0)) ELSE 0 END) AS IntraState_TaxableValue, " &
+             " SUM(CASE WHEN inv.INV_TYPE ='I' THEN ((BAL_ITEM_QTY * BAL_ITEM_RATE) - ISNULL(ITEM_DISCOUNT,0)) ELSE 0 END) AS InterState_TaxableValue " &
              " FROM    dbo.SALE_INVOICE_MASTER inv " &
              " INNER JOIN dbo.SALE_INVOICE_DETAIL invd ON invd.SI_ID = inv.SI_ID" &
              " INNER JOIN dbo.ACCOUNT_MASTER am ON am.ACC_ID = inv.CUST_ID " &
@@ -168,44 +168,69 @@ Public Class frm_GSTR_3
     End Function
 
     Private Function Get4_SectionDataForPurchaseReturn() As DataRow
-        Qry = " SELECT SUM(ISNULL(non_integrated_tax,0)) AS non_integrated_tax, " &
-            " SUM(ISNULL(integrated_tax,0)) As integrated_tax FROM (" &
-            " SELECT  SUM(CASE WHEN cm.STATE_ID = " & stateId & " THEN Item_vat ELSE 0 END) AS non_integrated_tax," &
-            "         SUM(CASE WHEN cm.STATE_ID != " & stateId & " THEN Item_vat ELSE 0 END) AS integrated_tax " &
-            " From dbo.ReverseMATERIAL_RECIEVED_Against_PO_MASTER ret" &
-            " inner Join dbo.MATERIAL_RECEIVED_AGAINST_PO_MASTER mrn ON ret.received_ID = mrn.Receipt_ID" &
-            " INNER Join dbo.ACCOUNT_MASTER am ON mrn.CUST_ID = am.ACC_ID" &
-            " INNER Join dbo.CITY_MASTER cm ON am.CITY_ID = cm.CITY_ID" &
-            " INNER Join dbo.ReverseMATERIAL_RECEIVED_Against_PO_DETAIL retd ON ret.Reverse_ID = retd.Reverse_ID" &
-            " WHERE MRN_STATUS <> 2 and MONTH(Reverse_Date) =  " & txtFromDate.Value.Month &
-            " And YEAR(Reverse_Date)=  " & txtFromDate.Value.Year &
-            " UNION" &
-            " SELECT  SUM(CASE WHEN cm.STATE_ID = " & stateId & " THEN Item_vat ELSE 0 END) AS non_integrated_tax," &
-            "         SUM(CASE WHEN cm.STATE_ID != " & stateId & " THEN Item_vat ELSE 0 END) AS integrated_tax " &
-            " From dbo.ReverseMATERIAL_RECIEVED_WITHOUT_PO_MASTER ret" &
-            " inner Join dbo.MATERIAL_RECIEVED_WITHOUT_PO_MASTER mrn ON ret.received_ID = mrn.Received_ID" &
-            " INNER Join dbo.ACCOUNT_MASTER am ON mrn.Vendor_ID = am.ACC_ID" &
-            " INNER Join dbo.CITY_MASTER cm ON am.CITY_ID = cm.CITY_ID" &
-            " INNER Join dbo.ReverseMATERIAL_RECEIVED_WITHOUT_PO_DETAIL retd ON ret.Reverse_ID = retd.Reverse_ID" &
-            " WHERE MRN_STATUS <> 2 and MONTH(Reverse_Date) =  " & txtFromDate.Value.Month &
-            " And YEAR(Reverse_Date)=  " & txtFromDate.Value.Year &
-            " ) AS temp"
+
+
+        'Qry = " SELECT SUM(ISNULL(non_integrated_tax,0)) AS non_integrated_tax, " &
+        '    " SUM(ISNULL(integrated_tax,0)) As integrated_tax FROM (" &
+        '    " SELECT  SUM(CASE WHEN cm.STATE_ID = " & stateId & " THEN Item_vat ELSE 0 END) AS non_integrated_tax," &
+        '    "         SUM(CASE WHEN cm.STATE_ID != " & stateId & " THEN Item_vat ELSE 0 END) AS integrated_tax " &
+        '    " From dbo.ReverseMATERIAL_RECIEVED_Against_PO_MASTER ret" &
+        '    " inner Join dbo.MATERIAL_RECEIVED_AGAINST_PO_MASTER mrn ON ret.received_ID = mrn.Receipt_ID" &
+        '    " INNER Join dbo.ACCOUNT_MASTER am ON mrn.CUST_ID = am.ACC_ID" &
+        '    " INNER Join dbo.CITY_MASTER cm ON am.CITY_ID = cm.CITY_ID" &
+        '    " INNER Join dbo.ReverseMATERIAL_RECEIVED_Against_PO_DETAIL retd ON ret.Reverse_ID = retd.Reverse_ID" &
+        '    " WHERE MRN_STATUS <> 2 and MONTH(Reverse_Date) =  " & txtFromDate.Value.Month &
+        '    " And YEAR(Reverse_Date)=  " & txtFromDate.Value.Year &
+        '    " UNION" &
+        '    " SELECT  SUM(CASE WHEN cm.STATE_ID = " & stateId & " THEN Item_vat ELSE 0 END) AS non_integrated_tax," &
+        '    "         SUM(CASE WHEN cm.STATE_ID != " & stateId & " THEN Item_vat ELSE 0 END) AS integrated_tax " &
+        '    " From dbo.ReverseMATERIAL_RECIEVED_WITHOUT_PO_MASTER ret" &
+        '    " inner Join dbo.MATERIAL_RECIEVED_WITHOUT_PO_MASTER mrn ON ret.received_ID = mrn.Received_ID" &
+        '    " INNER Join dbo.ACCOUNT_MASTER am ON mrn.Vendor_ID = am.ACC_ID" &
+        '    " INNER Join dbo.CITY_MASTER cm ON am.CITY_ID = cm.CITY_ID" &
+        '    " INNER Join dbo.ReverseMATERIAL_RECEIVED_WITHOUT_PO_DETAIL retd ON ret.Reverse_ID = retd.Reverse_ID" &
+        '    " WHERE MRN_STATUS <> 2 and MONTH(Reverse_Date) =  " & txtFromDate.Value.Month &
+        '    " And YEAR(Reverse_Date)=  " & txtFromDate.Value.Year &
+        '    " ) AS temp"
+
+
+
+        Qry = " SELECT ISNULL(SUM(non_integrated_tax), 0) AS non_integrated_tax ,
+       ISNULL(SUM(integrated_tax), 0) AS integrated_tax
+ FROM   ( SELECT    CASE WHEN sm.STATE_ID =" & stateId & "
+                         THEN ISNULL(Item_Tax, 0)
+                         ELSE 0
+                    END AS non_integrated_tax ,
+                    CASE WHEN sm.STATE_ID <> " & stateId & "
+                         THEN ISNULL(Item_Tax, 0)
+                         ELSE 0
+                    END AS integrated_tax
+          FROM      dbo.DebitNote_Master
+                    JOIN dbo.DebitNote_DETAIL ON dbo.DebitNote_Master.DebitNote_Id = dbo.DebitNote_DETAIL.DebitNote_Id
+                    JOIN dbo.ACCOUNT_MASTER ON ACC_ID = DN_CustId
+                    JOIN dbo.CITY_MASTER ON dbo.CITY_MASTER.CITY_ID = dbo.ACCOUNT_MASTER.CITY_ID
+                    JOIN dbo.STATE_MASTER sm ON sm.STATE_ID = dbo.CITY_MASTER.STATE_ID
+          WHERE     MONTH(DebitNote_Master.Creation_Date) =" & txtFromDate.Value.Month & "
+                    AND YEAR(DebitNote_Master.Creation_Date) = " & txtFromDate.Value.Year & "
+        ) tb"
+
+
         Return objCommFunction.Fill_DataSet(Qry).Tables(0).Rows(0)
     End Function
 
     Private Function Get4_SectionDataForPurchase() As DataRow
         Qry = " SELECT SUM(ISNULL(non_integrated_tax,0)) AS non_integrated_tax, " &
             " SUM(ISNULL(integrated_tax,0)) As integrated_tax FROM (" &
-            " SELECT  SUM(CASE WHEN cm.STATE_ID = " & stateId & " THEN mrn.GST_AMOUNT ELSE 0 END) AS non_integrated_tax," &
-            "         SUM(CASE WHEN cm.STATE_ID != " & stateId & " THEN mrn.GST_AMOUNT ELSE 0 END) AS integrated_tax " &
+            " SELECT  SUM(CASE WHEN mrn.MRN_TYPE <> 2 THEN mrn.GST_AMOUNT ELSE 0 END) AS non_integrated_tax," &
+            "         SUM(CASE WHEN mrn.MRN_TYPE = 2 THEN mrn.GST_AMOUNT ELSE 0 END) AS integrated_tax " &
             " FROM dbo.MATERIAL_RECIEVED_WITHOUT_PO_MASTER  AS mrn " &
             " INNER JOIN dbo.ACCOUNT_MASTER am ON mrn.Vendor_ID = am.ACC_ID" &
             " INNER JOIN dbo.CITY_MASTER cm ON cm.CITY_ID = am.CITY_ID" &
             " WHERE MRN_STATUS <> 2 and MONTH(Received_Date) =  " & txtFromDate.Value.Month &
             " And YEAR(Received_Date)=  " & txtFromDate.Value.Year &
             " union" &
-            " SELECT  SUM(CASE WHEN cm.STATE_ID = " & stateId & " THEN mrn.GST_AMOUNT ELSE 0 END) AS non_integrated_tax," &
-            "         SUM(CASE WHEN cm.STATE_ID != " & stateId & " THEN mrn.GST_AMOUNT ELSE 0 END) AS integrated_tax " &
+            " SELECT  SUM(CASE WHEN mrn.MRN_TYPE <> 2 THEN mrn.GST_AMOUNT ELSE 0 END) AS non_integrated_tax," &
+            "         SUM(CASE WHEN mrn.MRN_TYPE = 2 THEN mrn.GST_AMOUNT ELSE 0 END) AS integrated_tax " &
             " FROM dbo.MATERIAL_RECEIVED_AGAINST_PO_MASTER AS mrn " &
             " INNER JOIN dbo.ACCOUNT_MASTER am ON mrn.CUST_ID = am.ACC_ID" &
             " INNER JOIN dbo.CITY_MASTER cm ON cm.CITY_ID = am.CITY_ID" &
@@ -226,16 +251,16 @@ Public Class frm_GSTR_3
              " INNER JOIN dbo.STATE_MASTER sm ON sm.STATE_ID = cm.STATE_ID " &
              " WHERE MONTH(SI_DATE) =  " & txtFromDate.Value.Month &
              " And YEAR(SI_DATE) =  " & txtFromDate.Value.Year &
-             " And sm.STATE_ID != " & stateId &
-             " AND len(VAT_NO)=0 Group By STATE_CODE, STATE_NAME"
+             " AND inv.INV_TYPE='I' " &
+             " AND len(isnull(VAT_NO,''))=0 Group By STATE_CODE, STATE_NAME"
         Return objCommFunction.Fill_DataSet(Qry).Tables(0)
     End Function
 
     Private Function Get3_1_SectionData(condition As String) As DataRow
         Qry = " SELECT " &
              " isnull(SUM(((BAL_ITEM_QTY * BAL_ITEM_RATE) - ISNULL(ITEM_DISCOUNT,0))),0) As Taxable_Value, SUM(0) Cess_Amount, " &
-             " isnull(SUM(CASE WHEN sm.STATE_ID = " & stateId & " THEN invd.VAT_AMOUNT ELSE 0 END),0) AS non_integrated_tax, " &
-             " isnull(SUM(CASE WHEN sm.STATE_ID != " & stateId & " THEN invd.VAT_AMOUNT ELSE 0 END),0) AS integrated_tax " &
+             " isnull(SUM(CASE WHEN inv.INV_TYPE <>'I' THEN invd.VAT_AMOUNT ELSE 0 END),0) AS non_integrated_tax, " &
+             " isnull(SUM(CASE WHEN inv.INV_TYPE = 'I' THEN invd.VAT_AMOUNT ELSE 0 END),0) AS integrated_tax " &
              " FROM    dbo.SALE_INVOICE_MASTER inv " &
              " INNER JOIN dbo.SALE_INVOICE_DETAIL invd ON invd.SI_ID = inv.SI_ID" &
              " INNER JOIN dbo.ACCOUNT_MASTER am ON am.ACC_ID = inv.CUST_ID " &
@@ -248,7 +273,7 @@ Public Class frm_GSTR_3
     End Function
 
     Private Function GetBasicData() As DataRow
-        Qry = "SELECT OutletName_vch, TinNo_vch FROM dbo.OutletMaster"
+        Qry = "SELECT DIVISION_NAME, TIN_NO  FROM dbo.DIVISION_SETTINGS"
         Return objCommFunction.Fill_DataSet(Qry).Tables(0).Rows(0)
     End Function
 
